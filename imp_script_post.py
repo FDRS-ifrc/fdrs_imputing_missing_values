af4566f9b8689399b73027c4a5d8cafaf62c8483
Your Name <your.email@example.com>
2023-08-11 18:53:26 +0200

    Revamp and clean code for 2023 imputing push
    - Refactor core functions for efficiency
    - Clean and optimize code for clarity and performance
    - Update requirements.txt for wider dependency compatibility
    - Prepare for 2023 data imputing update

--- imp_script_post.py
+++ imp_script_post.py
@@ -1,111 +1,124 @@
-#Imputing Python script to be run each time imputed totals need to be updated
-#Note: staging to be replaced when backoffice and front end updated
-### 0. Import modules
+import os
+import json
+import time
+import requests
 import pandas as pd
 import numpy as np
-import random as rnd
-import requests, json
-from pandas import json_normalize
-import json
-from datetime import datetime
-print ("1. Getting Data")
-from fdrsapi import api_function,baseline, api_function_imputed
-f = open("public_api_key.txt", "r")
-api_key=f.readline()
-#Selecting years to download
-years=["2017","2018","2019","2020"]
-#Selecting indicators to impute
-kpi_code=["KPI_DonBlood_Tot","KPI_TrainFA_Tot","KPI_ReachDRER_D_Tot","KPI_ReachDRER_I","KPI_ReachDRR_D_Tot","KPI_ReachDRR_I","KPI_ReachLTSPD_D_Tot","KPI_ReachLTSPD_I","KPI_ReachS_D_Tot","KPI_ReachS_I","KPI_ReachL_D_Tot","KPI_ReachL_I","KPI_ReachM_D_Tot","KPI_ReachM_I","KPI_ReachCTP_D_Tot","KPI_ReachCTP_I","KPI_ReachSI_D_Tot","KPI_ReachSI_I","KPI_ReachH_D_Tot","KPI_ReachH_I","KPI_ReachWASH_I","KPI_ReachWASH_D_Tot","KPI_ReachDRER_CPD","KPI_ReachLTSPD_CPD","KPI_ReachDRR_CPD","KPI_ReachS_CPD","KPI_ReachL_CPD", "KPI_ReachH_CPD","KPI_ReachWASH_CPD","KPI_ReachM_CPD","KPI_ReachCTP_CPD","KPI_ReachSI_CPD","KPI_GB_Tot","KPI_pr_sex","KPI_sg_sex","KPI_PeopleVol_Tot","KPI_PStaff_Tot","KPI_noLocalUnits","KPI_IncomeLC","KPI_IncomeLC_CHF","KPI_expenditureLC","KPI_expenditureLC_CHF"]
-kpi_float=kpi_code.copy()
-kpi_float.remove("KPI_pr_sex")
-kpi_float.remove("KPI_sg_sex")
-time_series=api_function(years,kpi_code,kpi_float,api_key)
-print ("2. Running imputing missing values")
-from imp_function import imputing_mean,imputing_na
-# drop from 0 to 2011
-data_clean=time_series.query('KPI_Year >= 2012 & KPI_Year < 2021')
-# filter out columns that can be ignored: 64 columns dropped (see columns 1134 to 1070)
-# The only reporting year FDRS didn't count with 100% reporting was 2019. Therefore, lets divide the dataset in 2: 
-# 2012-2017, 2018-2020. This facilitates the code as KPI_WasSubmitted variable (which is being used to filter the data) 
-# was implemented in 2017 and all years before have NaN 
-# 1st subset:
-data_clean1=data_clean.query('KPI_Year >= 2012 & KPI_Year < 2018').copy()
-# 2nd subset:
-data_clean2=data_clean.query('KPI_Year >= 2018').copy()
-data_clean2_na_columns=data_clean2.copy()
-#Imputing:People reached sections 
-selected_cols=["KPI_DonBlood_Tot","KPI_TrainFA_Tot","KPI_ReachDRER_D_Tot","KPI_ReachDRER_I","KPI_ReachDRR_D_Tot","KPI_ReachDRR_I","KPI_ReachLTSPD_D_Tot","KPI_ReachLTSPD_I","KPI_ReachS_D_Tot","KPI_ReachS_I","KPI_ReachL_D_Tot","KPI_ReachL_I","KPI_ReachM_D_Tot","KPI_ReachM_I","KPI_ReachCTP_D_Tot","KPI_ReachCTP_I","KPI_ReachSI_D_Tot","KPI_ReachSI_I","KPI_ReachH_D_Tot","KPI_ReachH_I","KPI_ReachWASH_I","KPI_ReachWASH_D_Tot","KPI_ReachDRER_CPD","KPI_ReachLTSPD_CPD","KPI_ReachDRR_CPD","KPI_ReachS_CPD","KPI_ReachL_CPD", "KPI_ReachH_CPD","KPI_ReachWASH_CPD","KPI_ReachM_CPD","KPI_ReachCTP_CPD","KPI_ReachSI_CPD"]
-imputing_mean(data_clean2_na_columns,selected_cols,data_clean2)
-# Imputing : NS Finance and Parnership & NS Governance and Structure Section
-selected_cols_2d=["KPI_GB_Tot","KPI_pr_sex","KPI_sg_sex","KPI_PeopleVol_Tot","KPI_PStaff_Tot","KPI_noLocalUnits","KPI_IncomeLC_CHF" ,"KPI_expenditureLC_CHF"]
-# 18.11.21Comment : After reviewing with the Team, "supported1" and "received_support1" have been imputed seperatly in the script FDRS Network Data Transformation
-imputing_na(data_clean2_na_columns,selected_cols_2d)
-# concatenate the two sub_datasets
-fdrs_na_columns = [data_clean1, data_clean2_na_columns]
-fdrs_data_fdrs_na_columns = pd.concat(fdrs_na_columns)
+import sys
 
-#3. Get imputed values t-1 from backoffice to compare with imputed values t
-kpi_code=["KPI_PeopleVol_Tot","KPI_PStaff_Tot","KPI_DonBlood_Tot","KPI_TrainFA_Tot","KPI_noLocalUnits","KPI_ReachDRER_CPD","KPI_ReachLTSPD_CPD","KPI_ReachDRR_CPD","KPI_ReachS_CPD","KPI_ReachL_CPD","KPI_ReachH_CPD","KPI_ReachWASH_CPD","KPI_ReachM_CPD","KPI_ReachCTP_CPD","KPI_ReachSI_CPD","KPI_IncomeLC_CHF","KPI_expenditureLC_CHF"]
-kpi_code= [s + "_IP" for s in kpi_code]
-kpi_float=kpi_code.copy()
-time_series_imputed=api_function_imputed(years,kpi_code,kpi_float,api_key)
-time_series_imputed
-kpi_code=["KPI_PeopleVol_Tot","KPI_PStaff_Tot","KPI_DonBlood_Tot","KPI_TrainFA_Tot","KPI_noLocalUnits","KPI_ReachDRER_CPD","KPI_ReachLTSPD_CPD","KPI_ReachDRR_CPD","KPI_ReachS_CPD","KPI_ReachL_CPD","KPI_ReachH_CPD","KPI_ReachWASH_CPD","KPI_ReachM_CPD","KPI_ReachCTP_CPD","KPI_ReachSI_CPD","KPI_IncomeLC_CHF","KPI_expenditureLC_CHF"]
-# Convert to type int to remove decimals from calculations
-#fdrs_data_fdrs_na_columns[kpi_code]=fdrs_data_fdrs_na_columns[kpi_code].fillna(0).astype('int')
-#fdrs_data_fdrs_na_columns[kpi_code]=fdrs_data_fdrs_na_columns[kpi_code].replace(0,np.nan)
-kpi_code=["KPI_PeopleVol_Tot","KPI_PStaff_Tot","KPI_DonBlood_Tot","KPI_TrainFA_Tot","KPI_noLocalUnits","KPI_ReachDRER_CPD","KPI_ReachLTSPD_CPD","KPI_ReachDRR_CPD","KPI_ReachS_CPD","KPI_ReachL_CPD","KPI_ReachH_CPD","KPI_ReachWASH_CPD","KPI_ReachM_CPD","KPI_ReachCTP_CPD","KPI_ReachSI_CPD","KPI_IncomeLC_CHF","KPI_expenditureLC_CHF"]
-kpi_code_imp= [s + "_IP" for s in kpi_code]
-kpi_code_binary = [s + "_binary" for s in kpi_code]
-for kpi in list(kpi_code):
-    kpi_code_imp=kpi+"_IP"
-    kpi_code_binary=kpi+"_binary"
-    fdrs_data_fdrs_na_columns.loc[time_series_imputed[kpi_code_imp] == fdrs_data_fdrs_na_columns[kpi],kpi_code_binary]=0 #0 if same
-    fdrs_data_fdrs_na_columns.loc[time_series_imputed[kpi_code_imp] != fdrs_data_fdrs_na_columns[kpi],kpi_code_binary]=1 #1 if different
+# 1. Configuration Loading
+def select_environment(config_prod_path, config_staging_path):
+    """Select the environment and load the respective configuration."""
+    env = ''
+    while env not in ['staging', 'prod']:
+        env = input("Select environment ('staging' or 'prod'): ").lower()
+        if env == 'prod':
+            confirm = input("WARNING: You've selected the PRODUCTION environment. Are you sure? (yes/no) ")
+            if confirm.lower() != 'yes':
+                print("Switching to 'staging' by default for safety.")
+                env = 'staging'
+    try:
+        with open(config_prod_path if env == 'prod' else config_staging_path, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        raise Exception(f"Configuration file for {env} not found.")
+
+# Load configuration
+config_prod_path = '../src/config/config_prod.json'
+config_staging_path = '../src/config/config_staging.json'
+config = select_environment(config_prod_path, config_staging_path)
+print(f"Loaded configuration for {config['ENV']} environment.")
+user_email = input('Please provide your FDRS data analyst email: ')
+
+# 2. Data Retrieval
+sys.path.append(os.path.join(os.path.abspath(os.path.join('..')), "src", "data"))
+from fdrsapi import api_function
+
+# Load KPI Codebook
+base_url = config['BASE_URL']
+KPI = pd.json_normalize(data=requests.get(f"{base_url}indicator?apiKey={config['API_KEY_PUBLIC']}").json())
+codebook = KPI.copy()
+
+kpi_reach = codebook.query('KPI_Note == "NS Reach - CPD"')["KPI_Code"].to_list() + ["KPI_TrainFA_Tot", "KPI_DonBlood_Tot"]
+metadata_kpis = codebook.query('KPI_Note == "Metadata"')["KPI_Code"].tolist()
+kpi_gov = ["KPI_GB_Tot", "KPI_PeopleVol_Tot", "KPI_PStaff_Tot", "KPI_noLocalUnits"]
+kpi_fi = ["KPI_IncomeLC_CHF", "KPI_expenditureLC_CHF"]
+kpi_gov_fi_code = kpi_gov + kpi_fi
+exclude_kpis = ["KPI_Year", "DON_Code", "KPI_Id"]
+metadata_kpis = [kpi for kpi in metadata_kpis if kpi not in exclude_kpis]
+kpi_code = kpi_reach + kpi_gov_fi_code + metadata_kpis
+
+# Get data from FDRS API
+years = ["2018", "2019", "2020", "2021", "2022"]
+time_series = api_function(years, kpi_code, config)
+
+def impute_data(df, kpi_reach_code, kpi_gov_fi_code, selected_year):
+    """Impute missing data in the dataframe."""
+    data = df.copy()
     
-    fdrs_data_fdrs_na_columns.loc[fdrs_data_fdrs_na_columns[kpi].isna(),kpi_code_binary]=2
-    #To be uncommented when the data is ready
-    fdrs_data_fdrs_na_columns.loc[fdrs_data_fdrs_na_columns[kpi_code_binary]==2,kpi_code_binary]=0
+    condition_selected_year = data['KPI_Year'] == float(selected_year)
+    condition_not_submitted = data['KPI_WasSubmitted'] != 1.0
+    condition_nsgs_not_submitted = data['KPI_NSGS_WasSubmitted'] != 1.0
 
+    for col in kpi_reach_code:
+        mask = condition_selected_year & condition_not_submitted & data[col].isna()
+        if mask.any():
+            data[col] = data.groupby("NSO_DON_name")[col].transform(lambda x: x.fillna(x.mean(), limit=3))
+
+    for col in kpi_gov_fi_code:
+        mask = condition_selected_year & condition_not_submitted & data[col].isna()
+        if mask.any():
+            data[col] = data.groupby("NSO_DON_name")[col].transform(lambda x: x.fillna(method='ffill', limit=3))
+
+    condition_special = (data['KPI_WasSubmitted'] == 1.0) & condition_nsgs_not_submitted
+    for col in kpi_gov:
+        mask = condition_selected_year & condition_special & data[col].isna()
+        if mask.any():
+            data[col] = data.groupby("NSO_DON_name")[col].transform(lambda x: x.fillna(method='ffill', limit=3))
+
+    return data
+
+
+imputed_data_2022 = impute_data(time_series, kpi_reach, kpi_gov_fi_code, 2022)
+
+def prepare_data_for_post(original_df, imputed_df, kpi_list, selected_year):
+    """Prepare the data for posting."""
+    original_subset = original_df[original_df["KPI_Year"] == selected_year].copy()
+    imputed_subset = imputed_df[imputed_df["KPI_Year"] == selected_year].copy()
+
+    for kpi in kpi_list:
+        for _, row in imputed_subset.iterrows():
+            original_value = original_subset[original_subset['KPI_DON_code'] == row['KPI_DON_code']][kpi].values[0]
+            imputed_subset.at[row.name, f"{kpi}_source"] = 'NSI' if original_value == row[kpi] else 'I'
+
+    return imputed_subset
+
+def post_imputed_data(df, kpi_list, selected_year):
+    """Post imputed data."""
+    total_kpis = len(kpi_list)
+    total_dons = len(df["KPI_DON_code"].unique())
+    total_requests = total_kpis * total_dons
+    sleep_time = 0.5
+    print(f"Estimated time for completion: {total_requests * sleep_time / 60:.2f} minutes.")
+    
+    failed_posts = []
+    for kpi in kpi_list:
+        kpi_ip = kpi + "_IP"
+        for kpi_don_code in df["KPI_DON_code"].unique():
+            row = df[(df["KPI_Year"] == selected_year) & (df["KPI_DON_code"] == kpi_don_code)].iloc[0]
+            value = int(row[kpi]) if not pd.isna(row[kpi]) else ""
+            source = row[f"{kpi}_source"]
+            url = f"{config['BASE_URL']}ImputedKPI?apiKey={config['API_KEY_PRIVATE']}&kpicode={kpi_ip}&year={selected_year}&don_code={kpi_don_code}&value={value}&source={source}&user={user_email}"
             
+            response = requests.post(url)
+            time.sleep(sleep_time)
+            if response.status_code != 200:
+                failed_posts.append((kpi, kpi_don_code, response.status_code, response.text))
 
+    if failed_posts:
+        print("\nSummary of failed POSTs:")
+        for kpi, don, status, msg in failed_posts:
+            print(f"KPI: {kpi}, DON: {don} failed with {status}: {msg}")
 
-
-#Selecting years to post
-years=[2016,2017,2018,2019,2020]
-#Selecting indicators to impute
-f = open("api_key.txt", "r")
-api_key=f.readline()
-
-kpi_code=["KPI_PeopleVol_Tot"]
-payload={}
-files={}
-headers = {}
-
-#fdrs_data_fdrs_na_columns_test.fillna(0,inplace=True)
-for kpi in list(kpi_code):
-    kpi_binary = kpi + "_binary"
-    for year in years:
-        for kpi_don_code in list(set(fdrs_data_fdrs_na_columns["KPI_DON_code"])): 
-            for bi in (fdrs_data_fdrs_na_columns[kpi_binary][(fdrs_data_fdrs_na_columns["KPI_Year"]==year)&(fdrs_data_fdrs_na_columns["KPI_DON_code"]==kpi_don_code)]):
-                binary = bi
-                if binary == 1.0: 
-                    for label, content in (fdrs_data_fdrs_na_columns[kpi][(fdrs_data_fdrs_na_columns["KPI_Year"]==year)&(fdrs_data_fdrs_na_columns["KPI_DON_code"]==kpi_don_code)]).items():
-                        value = content
-                        if type(value)==float:
-                            if not np.isnan(value):
-                                value=np.int64(value)
-                                value = value
-                        #if value == "nan":
-                        if np.isnan(value):
-                            value = ""
-                        kpi_post=kpi+"_IP"
-                        url = (f"https://data-api-staging.ifrc.org/api/ImputedKPI?apiKey={api_key}&kpicode={kpi_post}&year={year}&don_code={kpi_don_code}&value={value}&user=simon.weiss@ifrc.org")
-                        r = requests.post(url,headers=headers, data=payload, files=files)
-                        #print(url,r.text)
-                        #print(r.request.body)
-                        #print(r.request.headers)
-
-df1 = pd.DataFrame([datetime.now()], columns=['Lattest Update Date'])
-df1.to_excel("lattest_update.xlsx", index=False)
-                        
+imputed_data_prepared = prepare_data_for_post(time_series, imputed_data_2022, kpi_reach + kpi_gov_fi_code, 2022)
+post_imputed_data(imputed_data_prepared, kpi_reach + kpi_gov_fi_code, 2022)
--- src/data/fdrsapi.py
+++ src/data/fdrsapi.py
@@ -1,86 +1,57 @@
-import pandas as pd
-import numpy as np
-import requests, json
+import requests
 from pandas import json_normalize
-import json
+import pandas as pd
 
+def baseline(years_selected, config):
+    """
+    Fetches baseline data for a given year from the FDRS API.
+    """
+    try:
+        code = requests.get(f"{config['BASE_URL']}entities/ns/?apiKey={config['API_KEY_PUBLIC']}").json()
+        df_code = json_normalize(data=code)[["KPI_DON_code", "NSO_DON_name", "NSO_ZON_name", "iso_3"]]
+        
+        dfs = [df_code.assign(KPI_Year=float(year)) for year in years_selected]
+        df_baseline = pd.concat(dfs, ignore_index=True)
+        df_baseline["KPI ID"] = df_baseline["KPI_DON_code"] + df_baseline["KPI_Year"].astype(str)
+        
+        return df_baseline
+    except requests.RequestException as e:
+        print(f"Error fetching baseline data: {e}")
+        return pd.DataFrame()
 
-#function baseline
-def baseline(years_selected,key):
-    code = requests.get(f"https://data-api.ifrc.org/api/entities/ns/?apiKey={key}").json()
-    df_code = json_normalize(data=code)
-    df_code=df_code[["KPI_DON_code","NSO_DON_name","NSO_ZON_name","iso_3"]]
-    df_baseline=pd.DataFrame()
-    df_final=pd.DataFrame()
-    for year in years_selected:
-        print(year)
-        df_code["KPI_Year"]=year
-        df_baseline=df_baseline._append(df_code)
-        df_final=df_final._append(df_baseline)
-    df_baseline["KPI ID"]=df_baseline["KPI_DON_code"]+df_baseline["KPI_Year"]
-    df_final["KPI ID"]=df_final["KPI_DON_code"]+df_final["KPI_Year"]
-    return df_baseline
-
-
-#api_function
-def api_function(years,kpi_code,kpi_float,api_key):
-    df_baseline=baseline(years,api_key)
+def fetch_data(years, kpi_code, config, endpoint):
     df_temp = pd.DataFrame()
     df = pd.DataFrame()
-    df_final=pd.DataFrame()
-    print("number of KPI downloaded:", len(kpi_code))
+
     for kpi_codes in kpi_code:
-    # df_final.merge(df_baseline[["KPI ID","value"]],on=["KPI ID"],how="outer")
         for year in years:
-            results = requests.get(f"https://data-api.ifrc.org/api/KpiValue?kpicode={kpi_codes}&year={year}&apiKey={api_key}&showunpublished=true").json()
-            df_temp = json_normalize(data=results)
-            df_temp["KPI_Year"] = year
-            df_temp["KPI_Code"]=kpi_codes
-            df=df._append(df_temp,ignore_index=True)
-            df["KPI ID"]=df["doncode"]+df["KPI_Year"]
-    df_final=df_baseline.merge(df[["KPI ID","value","KPI_Code"]],on=["KPI ID"],how="outer")
-    df_baseline.set_index("KPI ID",inplace=True)
-    df_final_pivot=df_final.pivot(values=["value"],columns=["KPI_Code"],index=["KPI ID"])
-    df_final_pivot["KPI ID"]=df_final_pivot.index
-    df_final_pivot.reset_index(drop=True)
-    df_final_pivot.index.rename('', inplace=True)
-    time_series=df_baseline.join(df_final_pivot["value"])
-    time_series=pd.DataFrame(time_series.to_records())
-    time_series=time_series.drop("nan",axis=1)
-    time_series[kpi_float]=time_series[kpi_float].astype(float)
-    time_series["KPI_Year"]=time_series["KPI_Year"].astype(np.int64)
-    return time_series
+            try:
+                url = f"{config['BASE_URL']}{endpoint}?kpicode={kpi_codes}&year={year}&apiKey={config['API_KEY_PUBLIC']}&showunpublished=true"
+                results = requests.get(url).json()
+                df_temp = json_normalize(data=results).assign(KPI_Year=float(year), KPI_Code=kpi_codes)
+                df = df._append(df_temp, ignore_index=True)
+            except requests.RequestException as e:
+                print(f"Error fetching data for {kpi_codes} in {year}: {e}")
 
+    return df
 
+def pivot_data(df, df_baseline):
+    df_pivoted = df.pivot_table(index=['doncode', 'KPI_Year'], columns='KPI_Code', values='value', aggfunc='first').reset_index()
     
-#api_function_imputed (for imputed data and imputed endpoint)
-def api_function_imputed(years,kpi_code,kpi_float,api_key):
-    df_baseline_2=baseline(years,api_key)
-    df_temp_2 = pd.DataFrame()
-    df_2 = pd.DataFrame()
-    df_final_2=pd.DataFrame()
-    print("number of KPI downloaded:", len(kpi_code))
-    for kpi_codes in kpi_code:
-    # df_final_2.merge(df_baseline_2[["KPI ID","value"]],on=["KPI ID"],how="outer")
-        for year in years:
-            results = requests.get(f"https://data-api.ifrc.org/api/KpiImputedValue?kpicode={kpi_codes}&year={year}&apiKey={api_key}&showunpublished=true").json()
-            df_temp_2 = json_normalize(data=results)
-            df_temp_2["KPI_Year"] = year
-            df_temp_2["KPI_Code"]=kpi_codes
-            df_2=df_2._append(df_temp_2,ignore_index=True)
-            df_2["KPI ID"]=df_2["doncode"]+df_2["KPI_Year"]
-    df_final_2=df_baseline_2.merge(df_2[["KPI ID","value","KPI_Code"]],on=["KPI ID"],how="outer")
-    df_baseline_2.set_index("KPI ID",inplace=True)
-    df_final_pivot_2=df_final_2.pivot(values=["value"],columns=["KPI_Code"],index=["KPI ID"])
-    df_final_pivot_2["KPI ID"]=df_final_pivot_2.index
-    df_final_pivot_2.reset_index(drop=True)
-    df_final_pivot_2.index.rename('', inplace=True)
-    time_series_imputed=df_baseline_2.join(df_final_pivot_2["value"])
-    time_series_imputed=pd.DataFrame(time_series_imputed.to_records())
-    #time_series_imputed=time_series_imputed.drop("nan",axis=1)
-    time_series_imputed[kpi_float]=time_series_imputed[kpi_float].astype(float)
-    time_series_imputed["KPI_Year"]=time_series_imputed["KPI_Year"].astype(np.int64)
-    return time_series_imputed
+    # Convert specific columns to float
+    for col in df_pivoted.columns:
+        if col not in ['doncode']:
+            df_pivoted[col] = df_pivoted[col].astype(float, errors='ignore')
+    
+    df_final = df_baseline.merge(df_pivoted, left_on=["KPI_DON_code", "KPI_Year"], right_on=["doncode", "KPI_Year"], how="outer")
+    return df_final.drop("doncode", axis=1)
 
+def api_function(years, kpi_code, config):
+    df_data = fetch_data(years, kpi_code, config, 'KpiValue')
+    df_baseline = baseline(years, config)
+    return pivot_data(df_data, df_baseline)
 
-
+def api_function_imputed(years, kpi_code, config):
+    df_data = fetch_data(years, kpi_code, config, 'KpiImputedValue')
+    df_baseline = baseline(years, config)
+    return pivot_data(df_data, df_baseline)
